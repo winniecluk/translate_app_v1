05f7fb604fc006f25c14fe88d1f102cd770207bb
Winnie Luk <winniecluk@gmail.com>
2016-09-21 09:45:42 -0700

    change static_pages to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "static_pages#index"
+  root to: "welcome#index"
 
 
   resources :users, except: [:index, :show, :edit]
