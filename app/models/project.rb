699caaa74fd7002db826e0336b24c70197b52131
Winnie Luk <winniecluk@gmail.com>
2016-09-22 20:38:28 -0700

    add user profile page

--- app/models/project.rb
+++ app/models/project.rb
@@ -20,25 +20,25 @@ class Project < ApplicationRecord
     User.find(vendor_id) if vendor_id
   end
 
-  # def needs
-  #   output_arr = []
-  #   output = ""
-  #   output_arr.push("need translator") if need_translator
-  #   output_arr.push("need transcriber") if need_transcriber
-  #   output_arr.push("need graphic designer") if need_graphic_designer
-  #   output_arr.push("need notarization") if need_notarization
-  #   if output_arr.length > 1
-  #     output_arr.each do |string, index|
-  #       if index == output_arr.length-1
-  #         output += string
-  #       else
-  #         output += string + ", "
-  #       end
-  #     end
-  #   else
-  #     output = output_arr[0]
-  #   end
-  #   return output
-  # end
+  def needs
+    output_arr = []
+    output = ""
+    output_arr.push("need translator") if need_translator
+    output_arr.push("need transcriber") if need_transcriber
+    output_arr.push("need graphic designer") if need_graphic_designer
+    output_arr.push("need notarization") if need_notarization
+    if output_arr.length > 1
+      output_arr.each do |string, index|
+        if index == output_arr.length-1
+          output += string
+        else
+          output += string + ", "
+        end
+      end
+    else
+      output = output_arr[0]
+    end
+    return output
+  end
 
 end
